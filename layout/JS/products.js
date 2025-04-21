c8ca6dcf7194f5dfe99ad29006428d9f8e6a2c54
3omda <3omda@iti>
2025-04-21 13:43:18 +0200

    pagination Logic

---
+++ layout/JS/products.js
@@ -0,0 +1,20 @@
+// let products = [];
+// const productsPerPage = 6;
+// let currentPage = 1;
+
+// function fetchProducts{
+    // fetch products here
+    // displayProducts(page);
+    // setupPagination();
+// }
+// function displayProducts(page){
+//     // create element and add products data
+// }
+
+// function setupPagination() {
+//     const pageCount = Math.ceil(products.length / productsPerPage);
+//     const pagination = document.getElementById("pagination");
+    
+// }
+
+//   fetchProducts();
